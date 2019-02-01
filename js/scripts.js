7531727a5e548b94f5a2cd62213ad9ec33ed6b00
Tanner Damron <tanner.mdamron@yahoo.com>
2019-02-01 10:01:34 -0800

    add multiple prototypes to calculate toppings cost and full cost of the pizza, add prototype to add text to price and append to page to show user the full price, finish functional basic pizza website

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 function Pizza(size, toppings, price) {
     this.size = size,
     this.toppings = [],
-    this.price = 0
+    this.price = 0,
+    this.toppingsPrice = 0
 }
 
 Pizza.prototype.addToppings = function (topping) {
@@ -19,6 +20,31 @@ Pizza.prototype.pizzaLogic = function () {
     };
 }
 
+Pizza.prototype.toppingsCost = function () {
+    if (document.getElementById("id_1").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_2").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_3").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_4").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_5").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_6").checked) {
+        this.toppingsPrice += 1;
+    } if (document.getElementById("id_7").checked) {
+        this.toppingsPrice += 1;
+    }
+}
+
+Pizza.prototype.pizzaCost = function () {
+    this.price += this.toppingsPrice;
+}
+
+Pizza.prototype.showFullPrice = function () {
+    return "Your order came to $" + this.price;
+}
 
 // User Interface Logic 
 $(document).ready(function() {
@@ -32,7 +58,9 @@ $(document).ready(function() {
         var newPizza = new Pizza(inputtedSize, inputtedToppings);
         newPizza.addToppings(inputtedToppings);
         newPizza.pizzaLogic();
+        newPizza.toppingsCost();
+        newPizza.pizzaCost();
+        $("#output").text(newPizza.showFullPrice());
         console.log(newPizza);
     });
-
 });
