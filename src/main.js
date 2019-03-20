cc68e171f61389645518dd3859d0bb1d2cc096e5
Clara Munro and Tanner Damron <clara+tanner@>
2019-03-20 16:04:42 -0700

    updated app for ES6

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import { Date, DatePrototype } from './weekday-calculator.js';
+import { Date } from './weekday-calculator.js';
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
@@ -7,20 +6,17 @@ import './styles.css';
 $(document).ready(function () {
   $("#weekdayForm").submit(function(event){
     event.preventDefault();
-    let userDate = $("#dateInput").val();
-    let dateSplit = DatePrototype.splitDate(userDate);
-    const yearNumber = parseInt(dateSplit[0]);
-    const monthNumber = parseInt(dateSplit[1]);
-    const dayNumber = parseInt(dateSplit[2]);
-    let dateObject = new Date(yearNumber, monthNumber, dayNumber);
-    dateObject.leapYearAddDays(yearNumber);
-    let finalDay = dateObject.getWeekday();
+    let countDays = 0;
+    let monthInput = $("#monthInput").val();
+    let dayInput = $("#dayInput").val();
+    let yearInput = $("#yearInput").val();
+    const dayNumber = parseInt(dayInput);
+    const monthNumber = parseInt(monthInput);
+    const yearNumber = parseInt(yearInput);
+    let dateObject = new Date(yearNumber, monthNumber, dayNumber, countDays);
+    let updatedDays = dateObject.leapYearAddDays(yearNumber, countDays);
+    let finalDay = dateObject.getWeekday(updatedDays);
     $("#result").text(finalDay);
-    console.log(dayNumber);
-    console.log(dateSplit);
-    console.log(userDate);
-    console.log(finalDay);
     dateObject.resetDays(yearNumber, monthNumber, dayNumber);
   });
 });
-/* eslint-disable no-unused-vars */
--- src/weekday-calculator.js
+++ src/weekday-calculator.js
@@ -1,71 +1,56 @@
 /* eslint-disable no-unused-vars */
-export function Date(year, month, day) {
-  this.year = year;
-  this.month = month;
-  this.day = day;
-}
+export class Date {
+  constructor(year, month, day, countDays) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+    this.countDays = countDays;
+  }
 
-export const DatePrototype = Date.prototype;
+  leapYearAddDays(year, countDays) {
+    let leapMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let notLeapMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    for (let i = 1; i <= parseInt(year) - 1; i++) {
+      if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
+        countDays += 366;
+      } else {
+        countDays += 365;
+      }
+    }
+    for (let m = 0; m < this.month - 1; m++) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        countDays += leapMonths[m];
+      }
+      else {
+        countDays += notLeapMonths[m];
+      }
+    }
+    for (let d = 0; d < this.day; d++) {
+      countDays += 1;
+    }
+    return countDays;
+  }
 
-const anchorDate = ["01", "01", "1800"];
+  resetDays() {
+    this.countDays = 0;
+  }
 
-let countDays = 0;
-
-Date.prototype.splitDate = function (date) {
-  let dateSplit = date.split("-");
-  return dateSplit;
-};
-
-Date.prototype.leapYearAddDays = function (year) {
-  let leapMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  let notLeapMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  for (let i = 1; i <= parseInt(year) - 1; i++) {
-    if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
-      countDays += 366;
+  getWeekday(countDays) {
+    if (countDays % 7 === 1) {
+      return "Monday";
+    } else if (countDays % 7 === 2) {
+      return "Tuesday";
+    } else if (countDays % 7 === 3) {
+      return "Wednesday";
+    } else if (countDays % 7 === 4) {
+      return "Thursday";
+    } else if (countDays % 7 === 5) {
+      return "Friday";
+    } else if (countDays % 7 === 6) {
+      return "Saturday";
     } else {
-      countDays += 365;
+      return "Sunday";
     }
   }
-  for (let m = 0; m < this.month - 1; m++) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-      countDays += leapMonths[m];
-    }
-    else {
-      countDays += notLeapMonths[m];
-    }
-  }
-  for (let d = 0; d < this.day; d++) {
-    countDays += 1;
-  }
-  console.log(countDays);
-  return countDays;
-};
-
-Date.prototype.resetDays = function () {
-  countDays = 0;
-};
-
-Date.prototype.getWeekday = function () {
-  if (countDays % 7 === 1) {
-    return "Monday";
-  } else if (countDays % 7 === 2) {
-    return "Tuesday";
-  } else if (countDays % 7 === 3) {
-    return "Wednesday";
-  } else if (countDays % 7 === 4) {
-    return "Thursday";
-  } else if (countDays % 7 === 5) {
-    return "Friday";
-  } else if (countDays % 7 === 6) {
-    return "Saturday";
-  } else {
-    return "Sunday";
-  }
-};
-
-
-Date.prototype.showDayOfWeek = function () {
-  return "The day of the week is ";
-};
-
+}
 /* eslint-disable no-unused-vars */
