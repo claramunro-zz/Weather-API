a4039dbc9347657559a05d13fb89ebab582dd4c3
Guest User <Guest@epicodus-7.local>
2019-03-27 08:52:30 -0700

    using jquery .get & .then

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+    let city = $('#location').val();
     $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cf769a9cbdb684773af3c84d2cee43fb`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   const getElements = function(response) {
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cf769a9cbdb684773af3c84d2cee43fb`).then(function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
   });
 });
